ee68a6c05d25f4d70ecafd2023a35f4ddb1221cf
Esteban Martín Cuadrado <emc2fuco@gmail.com>
2026-01-09 09:41:05 +0100

    Agregamos tres combinaciones de botones para ver resumenes

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
-let tirada = 0
 let piedras = 0
+let tirada = 0
 let papeles = 0
 let tijeras = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Piedras: " + piedras)
+})
 input.onGesture(Gesture.Shake, function () {
     tirada = randint(1, 3)
     if (tirada == 1) {
@@ -14,11 +17,12 @@ input.onGesture(Gesture.Shake, function () {
         basic.showIcon(IconNames.Scissors)
         tijeras = tijeras + 1
     }
+    basic.pause(5000)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showString("Piedras: " + piedras)
-    basic.pause(200)
-    basic.showString("Papeles: " + papeles)
-    basic.pause(200)
     basic.showString("Tijeras: " + tijeras)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Papeles: " + papeles)
 })
